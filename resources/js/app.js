522f4b72588b367f33f54b09625fa3be0a5c068f
luizsilvacaetano192 <luizsilvacaetano192@gmail.com>
2024-01-02 01:05:04 -0300

    b

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
 import {createApp} from 'vue';
 import App from './components/App.vue';
+import { VueMaskDirective } from 'v-mask';
 
-const app = createApp(App);
+const vMaskV2 = VueMaskDirective;
+const vMaskV3 = {
+    beforeMount: vMaskV2.bind,
+    updated: vMaskV2.componentUpdated,
+    unmounted: vMaskV2.unbind
+};
+
+const app = createApp(App).directive('mask', vMaskV3);
 
 app.mount('#app')
--- resources/js/components/ZipCode.vue
+++ resources/js/components/ZipCode.vue
@@ -9,7 +9,7 @@
                     <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                         <label for="zip-code" class="sm:px-3 py-4 sm:space-y-16 space-y-4 text-white block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">CEP</label>
                         <div class="mt-2 sm:col-span-2 sm:mt-0">
-                            <input v-mask ="'##/##/####'" type="text" name="zip-code" id="zip-code" v-model="zipCode" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                            <input v-mask ="'#####-###'" type="text" name="zip-code" id="zip-code" v-model="zipCode" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
                             <label for="zip-code" name="result-zip-code" id="result-zip-code"  class="text-white block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"> {{ resultZipCode }}</label>
                         </div>
                     </div>
